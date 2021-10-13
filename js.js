ef0dfba88b12122893e1ad72618c70e12506fe2a
Huzitatuguharu <sgrko.4751171@gmail.com>
2021-10-13 19:25:53 +0900

    headeradd

---
+++ js.js
@@ -0,0 +1,63 @@
+const App = {
+  init() {
+    console.log("Start the engines");
+
+    this.controllers.renderHeader();
+    this.controllers.renderContent();
+
+    this.controllers.renderFooter();
+    console.log("Finished");
+  },
+
+  state: {
+    navChild: [{ text: "Button" }, { text: "Accordion" }, { text: "Input" }],
+  },
+
+  controllers: {
+    renderHeader() {
+      const els = App.elements.header;
+      els.index.className = "header";
+      els.headerText.innerHTML = "ヘッダーです";
+      App.elements.app.appendChild(els.index);
+      els.index.appendChild(els.headerText);
+    },
+    renderContent() {
+      const els = App.elements.content;
+      els.index.className = "container__content";
+
+    },
+    renderFooter() {
+      const els = App.elements.footer;
+      els.index.className = "footer";
+      els.footerText.innerHTML = "ふったーです";
+      App.elements.app.appendChild(els.index);
+      els.index.appendChild(els.footerText);
+    },
+  },
+
+  elements: {
+    app: document.getElementById("app"),
+    container: document.createElement("div"),
+    content: {
+      index: document.createElement("main"),
+      cardBox: document.createElement("div"),
+      card: document.createElement("p"),
+
+    },
+    header: {
+      index: document.createElement("header"),
+      headerText: document.createElement("div"),
+    },
+    footer: {
+      index: document.createElement("footer"),
+      footerText: document.createElement("div"),
+    },
+    // a: document.createElement("a"),
+    // main: document.createElement("main"),
+    // messageJa: document.createElement("p"),
+    // messageEn: document.createElement("p"),
+    // nav: document.createElement("nav"),
+  },
+};
+
+App.init();
